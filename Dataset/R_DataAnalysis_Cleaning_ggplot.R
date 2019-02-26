8b09cdec9456088d73d36f05d50cb7993ffae75e
Evan Lih <Evan.Lih@Colorado.edu>
2019-02-26 09:56:15 -0800

    having issues reading into R. Testing different things

--- Dataset/R_DataAnalysis_Cleaning_ggplot.R
+++ Dataset/R_DataAnalysis_Cleaning_ggplot.R
@@ -1,1 +1,13 @@
 
+library(haven)
+library(ggplot2)
+library(scales)
+library(dplyr)
+library(curl)
+dataset <- read.csv("/Users/evanlih/Github/PUBPOL599-Seattle-Crisis-Data/Evan_Lih/Cleaned_RTW_Dataset")
+
+dataset <- ('https://raw.githubusercontent.com/EvanLih/PUBPOL599_Right_To_Work/master/Evan_Lih/Cleaned_RTW_Dataset')
+
+test <- read.csv(dataset)
+
+test <- read.csv(text=GET('https://raw.githubusercontent.com/EvanLih/PUBPOL599_Right_To_Work/master/Evan_Lih/Cleaned_RTW_Dataset'), header = T)

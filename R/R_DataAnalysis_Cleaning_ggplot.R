f9b2547554a8e3aac58300aaf05936f58649c393
Evan Lih <Evan.Lih@Colorado.edu>
2019-02-26 11:17:36 -0800

    updated filepath pull from online

--- R/R_DataAnalysis_Cleaning_ggplot.R
+++ R/R_DataAnalysis_Cleaning_ggplot.R
@@ -7,6 +7,6 @@ library(ggplot2)
 library(scales)
 library(dplyr)
 library(Rcurl)
-dataset <- 'https://github.com/EvanLih/PUBPOL599_Right_To_Work/raw/master/Evan_Lih/Cleaned_RTW_Dataset.csv'
+dataset <- 'https://github.com/EvanLih/PUBPOL599_Right_To_Work/raw/master/Dataset/Cleaned_RTW_Dataset.csv'
 
 x <- read.csv(dataset)

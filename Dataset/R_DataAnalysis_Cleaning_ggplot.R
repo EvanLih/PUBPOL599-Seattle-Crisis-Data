87f7aaa02c8db6feed8a0a141c877560462faea6
Evan Lih <Evan.Lih@Colorado.edu>
2019-02-26 11:00:52 -0800

    Moved important files to Dataset folder. Things should be working properly now. Please see D_DataAnalysis_Cleaning_ggplot.R for the beginning of the R coding

--- Dataset/R_DataAnalysis_Cleaning_ggplot.R
+++ Dataset/R_DataAnalysis_Cleaning_ggplot.R
@@ -1,11 +1,12 @@
 
+
+#This file is for reading the dataset into R. 
+#each of these calls these packages and installs them. We may need them to export more stuff in the future. 
 library(haven)
 library(ggplot2)
 library(scales)
 library(dplyr)
 library(Rcurl)
-dataset1 <- read.csv("/Users/evanlih/Github/PUBPOL599-Seattle-Crisis-Data/Evan_Lih/Cleaned_RTW_Dataset.csv")
+dataset <- 'https://github.com/EvanLih/PUBPOL599_Right_To_Work/raw/master/Evan_Lih/Cleaned_RTW_Dataset.csv'
 
-dataset <- ('https://raw.githubusercontent.com/EvanLih/PUBPOL599_Right_To_Work/master/Evan_Lih/Cleaned_RTW_Dataset.csv')
-
-x <- read.csv(dataset, header = TRUE)
+x <- read.csv(dataset)

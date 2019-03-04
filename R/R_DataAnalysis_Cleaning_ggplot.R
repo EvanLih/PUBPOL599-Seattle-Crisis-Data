8e08cfa7daf3c4d42a956c5c267377977599c5d1
Evan Lih <Evan.Lih@Colorado.edu>
2019-03-04 12:40:46 -0800

    created code for preliminary boxplot under R_DataAnalysis_Cleaning_ggplot.R

--- R/R_DataAnalysis_Cleaning_ggplot.R
+++ R/R_DataAnalysis_Cleaning_ggplot.R
@@ -2,11 +2,25 @@
 
 #This file is for reading the dataset into R. 
 #each of these calls these packages and installs them. We may need them to export more stuff in the future. 
-library(haven)
-library(ggplot2)
-library(scales)
-library(dplyr)
-library(Rcurl)
+install.packages("pacman")
+pacman::p_load("haven", "ggplot2", "scales", 
+               "dplyr", "Rcurl", "magrittr")
+
 dataset <- 'https://github.com/EvanLih/PUBPOL599_Right_To_Work/raw/master/Dataset/Cleaned_RTW_Dataset.csv'
 
-x <- read.csv(dataset)
+dataset <- read.csv('/Users/evanlih/Github/PUBPOL599-Seattle-Crisis-Data/Dataset/Cleaned_RTW_Dataset.csv')
+
+#converting the Right_to_Work column into as a factor, as it is currently being read as a integer. 
+dataset$Right_to_Work %<>% as.factor
+
+ggplot(dataset, aes(Right_to_Work, incwage), group = Right_to_Work) +
+  geom_boxplot(outlier.color = "red", outlier.shape = 1) +
+  scale_y_continuous(breaks = c(10000,30000,50000,70000,100000,500000), labels = dollar) +
+  labs(x = "Right to Work", y = "Income (dollars)") +
+  geom_text(data=means, aes(label = incwage, y = incwage + .09)) +
+  scale_x_discrete(breaks= c(0,1), labels = c("No Right to Work", "Right to Work"))
+  
+means <- aggregate(incwage ~ Right_to_Work, dataset, mean)
+means$incwage %<>% round(2)
+#geom_bar(stat = "identity") +
+  
